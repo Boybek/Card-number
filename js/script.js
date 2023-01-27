c9ced0569dc58817263ecd761a653976d6b2341b
Bobomurodov Oybek[FE17] <122271272+Boybek@users.noreply.github.com>
2023-01-27 17:49:52 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,3 +8,16 @@
 
   //8600 ******** 9101
 
+
+
+  function repearStr(str, num){
+    for (let i = 0; i < num; i++){
+         console.log(str +(i+1))
+
+
+    }
+}
+
+let a = "hello "
+repearStr(a, 6)
+
